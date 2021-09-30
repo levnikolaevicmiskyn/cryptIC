ac83ecaa2fe3c57ac8c655db88bfc23445ea7cb7
Alessandro Rocco Scisca <alessandrorocco.scisca@studenti.polito.it>
2021-09-30 20:29:20 +0200

    fix variable size error

--- test/arduino_firmware/arduino_firmware.ino
+++ test/arduino_firmware/arduino_firmware.ino
@@ -13,8 +13,8 @@
 #define CRYPTIC_BUF_LEN SHA256_BLOCK_SIZE*CRYPTIC_N_BLOCKS
 
 /**************************** DATA TYPES ****************************/
-typedef unsigned char BYTE;             // 8-bit byte
-typedef unsigned int  WORD;             // 32-bit word, change to "long" for 16-bit machines
+typedef byte BYTE;             // 8-bit byte
+typedef u32  WORD;             // 32-bit word, change to "long" for 16-bit machines
 
 typedef struct {
   BYTE data[64];
@@ -58,8 +58,12 @@ void sha256_transform(SHA256_CTX *ctx, const BYTE data[])
 {
   WORD a, b, c, d, e, f, g, h, i, j, t1, t2, m[64];
 
-  for (i = 0, j = 0; i < 16; ++i, j += 4)
-    m[i] = (data[j] << 24) | (data[j + 1] << 16) | (data[j + 2] << 8) | (data[j + 3]);
+  for (i = 0, j = 0; i < 16; ++i, j += 4) {
+    m[i] =    ((WORD) data[j] << 24) 
+            | ((WORD) data[j + 1] << 16) 
+            | ((WORD) data[j + 2] << 8) 
+            | ((WORD) data[j + 3]);
+  }
   for ( ; i < 64; ++i)
     m[i] = SIG1(m[i - 2]) + m[i - 7] + SIG0(m[i - 15]) + m[i - 16];
 
